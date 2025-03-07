882965421452c65ae601806442262c16ba0cfab4
Subhanandhini-760 <Subhanandhinivenkatesan@gmail.com>
2025-03-07 11:41:06 +0530

    palindrome.java

--- age.java
+++ age.java
@@ -1,14 +1,29 @@
-class age {
-int b;
-age()
-{
-System.out.println("constructor call");
-b=50;
-}
-public static void main(String args[])
-{
-age obj = new age();
-System.out.println("The number is:"+obj.b);
-}
+import java.util.Scanner;
+
+public class PalindromeChecker {
+    public static boolean isPalindrome(String str) {
+        int left = 0, right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        scanner.close();
+        
+        if (isPalindrome(input)) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }
+    }
 }
 
